9fb5af11c65a4c5f382bdd036aaf13e0e4153d67
mariojr04 <162380419+mariojr04@users.noreply.github.com>
2025-06-05 17:39:04 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,88 +1,97 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int opcao;
 
         do {
-            System.out.println("\n===== MENU PRINCIPAL =====");
-            System.out.println("1. Cadastrar Hóspede");
-            System.out.println("2. Listar Hóspedes");
-            System.out.println("3. Cadastrar Funcionário");
-            System.out.println("4. Listar Funcionários");
+            System.out.println("\n=== MENU PRINCIPAL ===");
+            System.out.println("1. Inserir Funcionário");
+            System.out.println("2. Inserir Hóspede");
+            System.out.println("3. Listar Funcionários");
+            System.out.println("4. Listar Hóspedes");
             System.out.println("0. Sair");
-            System.out.print("Escolha uma opção: ");
-
-            opcao = Integer.parseInt(scanner.nextLine());
+            System.out.print("Escolha: ");
+            opcao = sc.nextInt();
 
             switch (opcao) {
                 case 1:
+                    sc.nextLine();
                     System.out.print("CPF: ");
-                    String cpf = scanner.nextLine();
+                    String cpfF = sc.nextLine();
                     System.out.print("Nome: ");
-                    String nome = scanner.nextLine();
+                    String nomeF = sc.nextLine();
                     System.out.print("Idade: ");
-                    int idade = Integer.parseInt(scanner.nextLine());
-                    System.out.print("RG: ");
-                    String rg = scanner.nextLine();
-                    System.out.print("Fidelidade (true/false): ");
-                    boolean fidelidade = Boolean.parseBoolean(scanner.nextLine());
-
-                    Hospede hospede = new Hospede(cpf, nome, idade, rg, fidelidade);
-                    if (hospede.Inserir()) {
-                        System.out.println("Hóspede cadastrado com sucesso.");
+                    int idadeF = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Função: ");
+                    String funcao = sc.nextLine();
+                    Funcionario f = new Funcionario(cpfF, nomeF, idadeF, funcao);
+                    if (f.Inserir()) {
+                        System.out.println("Funcionário inserido com sucesso!");
+                    } else {
+                        System.out.println("Falha ao inserir funcionário.");
                     }
                     break;
-
-
 
                 case 2:
-                    Hospede hTemp = new Hospede("", "", 0, "", false);
-                    for (Hospede h : hTemp.Listar()) {
-                        h.ToString();
-                        System.out.println("----------------------");
+                    sc.nextLine();
+                    System.out.print("CPF: ");
+                    String cpfH = sc.nextLine();
+                    System.out.print("Nome: ");
+                    String nomeH = sc.nextLine();
+                    System.out.print("Idade: ");
+                    int idadeH = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("RG: ");
+                    String rg = sc.nextLine();
+                    System.out.print("Fidelidade (true/false): ");
+                    Boolean fidelidade = sc.nextBoolean();
+                    Hospede h = new Hospede(cpfH, nomeH, idadeH, rg, fidelidade);
+                    if (h.Inserir()) {
+                        System.out.println("Hóspede inserido com sucesso!");
+                    } else {
+                        System.out.println("Falha ao inserir hóspede.");
                     }
                     break;
-
-
 
                 case 3:
-                    System.out.print("CPF: ");
-                    String cpfF = scanner.nextLine();
-                    System.out.print("Nome: ");
-                    String nomeF = scanner.nextLine();
-                    System.out.print("Idade: ");
-                    int idadeF = Integer.parseInt(scanner.nextLine());
-                    System.out.print("Função: ");
-                    String funcao = scanner.nextLine();
-
-                    Funcionario funcionario = new Funcionario(cpfF, nomeF, idadeF, funcao);
-                    if (funcionario.Inserir()) {
-                        System.out.println("Funcionário cadastrado com sucesso.");
+                    Funcionario fAux = new Funcionario("", "", 0, "");
+                    ArrayList<Funcionario> listaF = fAux.listar();
+                    if (listaF.isEmpty()) {
+                        System.out.println("Nenhum funcionário cadastrado.");
+                    } else {
+                        System.out.println("\n=== Lista de Funcionários ===");
+                        for (Funcionario func : listaF) {
+                            func.toString();
+                        }
                     }
                     break;
-
-
 
                 case 4:
-                    Funcionario fTemp = new Funcionario("", "", 0, "");
-                    for (Funcionario f : fTemp.Listar()) {
-                        f.ToString();
-                        System.out.println("----------------------");
+                    Hospede hAux = new Hospede("", "", 0, "", false);
+                    ArrayList<Hospede> listaH = hAux.listar();
+                    if (listaH.isEmpty()) {
+                        System.out.println("Nenhum hóspede cadastrado.");
+                    } else {
+                        System.out.println("\n=== Lista de Hóspedes ===");
+                        for (Hospede hosp : listaH) {
+                            hosp.ToString();
+                        }
                     }
                     break;
-
-
 
                 case 0:
                     System.out.println("Saindo...");
                     break;
+
                 default:
-                    System.out.println("Opção inválida. Tente novamente.");
+                    System.out.println("Opção inválida!");
             }
         } while (opcao != 0);
 
-        scanner.close();
+        sc.close();
     }
 }
